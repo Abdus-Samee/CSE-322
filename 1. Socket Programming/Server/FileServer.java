c71de6970fa9c3cf4dc7c1dcf56e74bac9bba9e5
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-21 12:26:25 +0600

    Create FileServer.java

---
+++ 1. Socket Programming/Server/FileServer.java
@@ -0,0 +1,26 @@
+package Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class FileServer {
+    static final int PORT = 5021;
+    static final String rootPath = "root/";
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+        while(true){
+            Socket socket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            new Connection(socket, in, pw, rootPath).start();
+        }
+    }
+
+    public static void listFileAndDir(){
+
+    }
+}
