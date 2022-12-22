5dcd622a73de73d4e2d50a6a32d65c18cfd4bc48
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-22 20:17:18 +0600

    Update FileServer.java

--- 1. Socket Programming/Server/FileServer.java
+++ 1. Socket Programming/Server/FileServer.java
@@ -14,7 +14,7 @@ public class FileServer {
 
         while(true){
             Socket socket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            InputStream in = socket.getInputStream();
             OutputStream pw = socket.getOutputStream();
             new Connection(socket, in, pw, rootPath).start();
         }
