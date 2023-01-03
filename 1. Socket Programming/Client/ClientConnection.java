ab150b045fd25163a5aec10167e325396afc8ceb
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-03 22:43:17 +0600

    Update ClientConnection.java

--- 1. Socket Programming/Client/ClientConnection.java
+++ 1. Socket Programming/Client/ClientConnection.java
@@ -11,7 +11,7 @@ public class ClientConnection extends Thread{
     private OutputStream os;
     static final String FILE_PATH = "src/Client/dir/";
     static final int CHUNK_SIZE = 50;
-    private String[] extensions = {".txt", ".pdf", ".jpg", ".png", ".bmp", ".mp4"};
+    private String[] extensions = {"txt", "pdf", "jpg", "png", "bmp", "mp4"};
 
     public ClientConnection(Socket socket, String filename) throws Exception{
         this.filename = filename;
@@ -24,26 +24,41 @@ public class ClientConnection extends Thread{
         return Arrays.stream(extensions).anyMatch(entry -> s.endsWith(entry));
     }
 
-    public void uploadFile(File file) throws Exception{
-        FileInputStream fis = new FileInputStream(file);
+    public void uploadFile(File file, int found) throws Exception{
+        FileInputStream fis = null;
         String type = "";
         if(file.getName().toLowerCase().endsWith(".pdf")) type = "application/pdf";
         else if(file.getName().toLowerCase().endsWith(".docx")) type = "application/msword";
 
-        String response = "UPLOAD " + this.filename + "\r\n" +
-                          "Content-Type: " + type + "\r\n" +
-                          "Content-Disposition: attachment; filename=" + file.getName() + "\r\n" +
-                          "Content-Length: " + fis.available() + "\r\n\r\n";
+        String response = "";
+        if(found == 1){
+            fis = new FileInputStream(file);
+            response += "UPLOAD " + this.filename + "\r\n" +
+                    "Content-Type: " + type + "\r\n" +
+                    "Content-Disposition: attachment; filename=" + file.getName() + "\r\n" +
+                    "Content-Length: " + fis.available() + "\r\n\r\n";
+        }else if(found == 0){
+            response += "UPLOAD " + this.filename + "_format" + "\r\n\r\n\r\n";
+        }else{
+            response += "UPLOAD " + this.filename + "_name" + "\r\n\r\n\r\n";
+        }
 
         os.write(response.getBytes());
 
-        int count;
-        byte[] buffer = new byte[CHUNK_SIZE];
-        while ((count = fis.read(buffer)) > 0) os.write(buffer, 0, count);
+        if(found == 1){
+            int count;
+            byte[] buffer = new byte[CHUNK_SIZE];
+            while ((count = fis.read(buffer)) > 0) os.write(buffer, 0, count);
+        }
 
         os.flush();
-        fis.close();
-        System.out.println("uploaded");
+        if(found == 1) fis.close();
+
+        if(found == 1) System.out.println("Uploading file " + this.filename + ": successful");
+        else if(found == 0) System.out.println("Uploading file " + this.filename + ": invalid file format");
+        else System.out.println("Uploading file " + this.filename + ": invalid file name");
+
+        closeConnection();
     }
 
     public void closeConnection(){
@@ -57,24 +72,25 @@ public class ClientConnection extends Thread{
     @Override
     public void run(){
         try{
-            boolean found = false;
+            int found = -1;
             File dir = new File(FILE_PATH);
             File reqd = new File(FILE_PATH, this.filename);
 
-            File[] files = dir.listFiles((file, s) -> checkIfFileHasExtension(s, extensions));
+            //File[] files = dir.listFiles((file, s) -> checkIfFileHasExtension(s, extensions));
+            File[] files = dir.listFiles();
 
             for(File f : files){
                 if(f.equals(reqd)){
-                    found = true;
+                    if(checkIfFileHasExtension(f.getName().split("[.]", 0)[1], extensions)) found = 1;
+                    else found = 0;
+                    break;
                 }
             }
 
-            if(!found) System.out.println("File not found... ...");
-
-            uploadFile(reqd);
+            uploadFile(reqd, found);
         }catch(Exception e){
             closeConnection();
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
