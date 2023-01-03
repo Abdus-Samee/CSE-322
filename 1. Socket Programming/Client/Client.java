24fd009430a54be12435ab580ac260f3df7f91c8
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-03 22:42:03 +0600

    Update Client.java

--- 1. Socket Programming/Client/Client.java
+++ 1. Socket Programming/Client/Client.java
@@ -7,12 +7,12 @@ public class Client {
     static final int PORT = 5021;
 
     public static void main(String[] args) throws Exception {
-        while(true){
+        System.out.println("Enter filenames separated by spaces:");
+        Scanner scanner = new Scanner(System.in);
+        String[] in = scanner.nextLine().split(" ");
+        for(String s : in){
             Socket socket = new Socket("127.0.0.1", PORT);
-            System.out.println("Enter filename:");
-            Scanner scanner = new Scanner(System.in);
-            String in = scanner.nextLine();
-            new ClientConnection(socket, in).start();
+            new ClientConnection(socket, s).start();
         }
     }
 }
