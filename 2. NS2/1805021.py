d6bf9e2b06eea638271c232bb4dcace7e23c8e94
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-27 00:37:25 +0600

    Update 1805021.py

--- 2. NS2/1805021.py
+++ 2. NS2/1805021.py
@@ -2,5 +2,16 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(sys.argv[1], header=None, delimiter=',', names=["first", "second", "third", "fourth"])
+df = pd.read_csv(sys.argv[1], header=None, delimiter=',', names=["first", "second", "third", "fourth", "fifth"], skiprows=1)
 print(df["second"])
+
+data = df.values.tolist()
+print(data)
+
+if sys.argv[1] == "area.txt":
+    plt.plot(data, [[250, 500, 750, 1000, 1250]], color="red", label="area", marker="o")
+    plt.title("Area Graph")
+    plt.legend()
+    plt.xlabel("Values")
+    plt.ylabel("Data")
+    plt.show()
