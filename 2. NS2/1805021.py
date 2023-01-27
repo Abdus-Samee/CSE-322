e4b263ee4ebc5f31ee35ed3387f91867e4e2cafc
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-28 00:24:10 +0600

    Update 1805021.py

--- 2. NS2/1805021.py
+++ 2. NS2/1805021.py
@@ -1,4 +1,4 @@
-# python3 1805021.py area.txt
+# python3 1805021.py areas.txt
 
 import sys
 import pandas as pd
@@ -15,36 +15,40 @@ name = sys.argv[1][:-4]
 l = globals()[name]
 data = df["first"].values.flatten().tolist()
 # print(data)
-plt.plot(l, data, color="red", label="area", marker="o")
+plt.plot(l, data, color="purple", label="area", marker="o")
 plt.title("Throughput vs " + name.capitalize())
-plt.legend()
-plt.xlabel("Area")
+plt.xlabel(name.capitalize())
 plt.ylabel("Throughput(bits/s)")
+plt.xticks(l, [str(x) for x in l])
+plt.yticks(data, [str(round(x, 6)) for x in data], rotation=45)
 plt.show()
 
 data = df["second"].values.flatten().tolist()
 # print(data)
 plt.plot(l, data, color="red", label="area", marker="o")
 plt.title("Delay vs " + name.capitalize())
-plt.legend()
-plt.xlabel("Area")
+plt.xlabel(name.capitalize())
 plt.ylabel("Delay(s)")
+plt.xticks(l, [str(x) for x in l])
+plt.yticks(data, [str(round(x, 6)) for x in data], rotation=45)
 plt.show()
 
 data = df["third"].values.flatten().tolist()
 # print(data)
-plt.plot(l, data, color="red", label="area", marker="o")
+plt.plot(l, data, color="blue", label="area", marker="o")
 plt.title("Delivery Ratio vs " + name.capitalize())
-plt.legend()
-plt.xlabel("Area")
+plt.xlabel(name.capitalize())
 plt.ylabel("Delivery Ratio")
+plt.xticks(l, [str(x) for x in l])
+plt.yticks(data, [str(round(x, 6)) for x in data], rotation=45)
 plt.show()
 
 data = df["fourth"].values.flatten().tolist()
 # print(data)
-plt.plot(l, data, color="red", label="area", marker="o")
+plt.plot(l, data, color="green", label="area", marker="o")
 plt.title("Drop Ratio vs " + name.capitalize())
-plt.legend()
-plt.xlabel("Area")
+plt.xlabel(name.capitalize())
 plt.ylabel("Drop Ratio")
+plt.xticks(l, [str(x) for x in l])
+plt.yticks(data, [str(round(x, 6)) for x in data], rotation=45)
 plt.show()
