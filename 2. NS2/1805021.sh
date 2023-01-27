ea2cfbf099a1360f0376a22735cf68aaeea3a551
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-27 17:27:41 +0600

    Update 1805021.sh

--- 2. NS2/1805021.sh
+++ 2. NS2/1805021.sh
@@ -4,7 +4,7 @@ tcl_file="1805021.tcl"
 trace_file="1805021.tr"
 awk_file="1805021.awk"
 
-output_area="area.txt"
+output_area="areas.txt"
 output_nodes="nodes.txt"
 output_flows="flows.txt"
 
@@ -12,9 +12,26 @@ declare -i area=(250 500 750 1000 1250)
 declare -i nodes=(20 40 60 80 100)
 declare -i flows=(10 20 30 40 50)
 
-echo "Throughput(bits/s),Delay(s),Delivery Ratio, Drop Ratio" > $output_area
-# traverse the area array
+echo "Throughput(bits/s), Delay(s), Delivery Ratio, Drop Ratio" > $output_area
 for i in "${area[@]}";do
     ns $tcl_file -x $i
     awk -f $awk_file $trace_file >> $output_area
 done
+
+echo "Area file generation done!"
+
+echo "Throughput(bits/s), Delay(s), Delivery Ratio, Drop Ratio" > $output_nodes
+for i in "${nodes[@]}";do
+    ns $tcl_file -nodes $i
+    awk -f $awk_file $trace_file >> $output_nodes
+done
+
+echo "Node file generation done!"
+
+echo "Throughput(bits/s), Delay(s), Delivery Ratio, Drop Ratio" > $output_flows
+for i in "${flows[@]}";do
+    ns $tcl_file -flows $i
+    awk -f $awk_file $trace_file >> $output_flows
+done
+
+echo "Flow file generation done!"
